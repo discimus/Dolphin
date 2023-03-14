d7ff01c577277473ad3ca24c730d8ab28416008a
discimus <newillianpereira@gmail.com>
2023-03-13 21:13:18 -0300

    first commit

---
+++ Dolphin/Controllers/GreetController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Dolphin.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class GreetController : Controller
+    {
+        [HttpGet]
+        public string Index()
+        {
+            return "Hello, World!";
+        }
+    }
+}
